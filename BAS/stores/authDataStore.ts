132f6226071dd749e4f2b95061c6ada862df4ead
zinminthet <zinminthet.se@gmail.com>
2025-10-08 21:44:03 +0630

    feat(state): add userId in the global state to map records to that id

--- BAS/stores/authDataStore.ts
+++ BAS/stores/authDataStore.ts
@@ -3,14 +3,17 @@ import * as Keychain from "react-native-keychain";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 type loginStore = {
-  rollNumber: string;
+  userId: string | null;
+  rollNumber: string | null;
   token: string | null;
+  isLoggedIn: boolean;
   profileData: {
     name: null | string;
     phoneNumber: null | string;
     email: null | string;
     rollNumber: null | string;
     university: null | string;
+    userId: null | string;
   };
   login: (rollNumber: string, password: string, token: string) => Promise<void>;
   logout: () => Promise<void>;
@@ -19,13 +22,17 @@ type loginStore = {
     phoneNumber: string,
     email: string,
     rollNumber: string,
-    university: string
+    university: string,
+    userId: string
   ) => Promise<void>;
 };
 
 export const useLoginStore = create<loginStore>((set) => ({
-  rollNumber: "0",
+  userId: null,
+  rollNumber: null,
+  isLoggedIn: false,
   profileData: {
+    userId: null,
     name: null,
     phoneNumber: null,
     rollNumber: null,
@@ -41,24 +48,28 @@ export const useLoginStore = create<loginStore>((set) => ({
       service: "account_password",
     });
 
-    set({ token, rollNumber: rollNumber });
+    set({ token, rollNumber: rollNumber, isLoggedIn: true });
   },
-  hydrate: async (name, phoneNumber, email, rollNumber, university) => {
+  hydrate: async (name, phoneNumber, email, rollNumber, university, userId) => {
     const profileData = {
       name,
       phoneNumber,
       rollNumber,
       email,
       university,
+      userId,
     };
     await AsyncStorage.setItem("@profileData", JSON.stringify(profileData));
     set({ profileData: profileData });
   },
   logout: async () => {
     set({
+      userId: null,
       token: null,
-      rollNumber: "0",
+      rollNumber: null,
+      isLoggedIn: false,
       profileData: {
+        userId: null,
         name: null,
         email: null,
         phoneNumber: null,
